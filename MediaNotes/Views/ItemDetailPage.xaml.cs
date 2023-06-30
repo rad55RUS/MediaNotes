4f882023d6e0389f536bd25a209f3e19b34e30a2
rad55RUS <radel-02@inbox.ru>
2023-06-30 22:07:14 +0300

    Добавьте файлы проекта.

---
+++ MediaNotes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MediaNotes.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MediaNotes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
